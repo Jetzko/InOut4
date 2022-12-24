ca4c84fd5c506308fba73961443fe58d3caff360
Jetzko <andreadefilippi68@gmail.com>
2022-12-24 09:48:32 +0100

    Input Output 4

---
+++ src/Start.java
@@ -0,0 +1,8 @@
+public class Start {
+    public static void main(String[] args) {
+       String name = "ANDREA";
+       System.out.printf("%s" + "%n", name);
+       System.out.printf("%f", 3.141590);
+
+    }
+}
